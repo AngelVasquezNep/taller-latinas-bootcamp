e6ff1afc03c7f95e98d972ece2b5f7cea8e935a1
Ángel Vásquez <angel.vasquez.unam@gmail.com>
2021-04-18 15:13:04 -0500

    Styles manipulation

---
+++ src/js/playground.js
@@ -0,0 +1,14 @@
+import { colorElementRed, updateElementStyles } from "./utils.js";
+
+/** ---------------------------------------------------------------------
+ *
+ * Styles manipulation
+ *
+ */
+
+// colorElementRed("Angelito");
+
+// updateElementStyles(".Contacts .Contact", {
+//   border: "2px solid green",
+// });
+
