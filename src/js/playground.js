723855ecbab158c9c6642fdc5aed4644a297c9ee
Ángel Vásquez <angel.vasquez.unam@gmail.com>
2021-04-18 15:13:31 -0500

    Fake dark mode

--- src/js/playground.js
+++ src/js/playground.js
@@ -12,3 +12,35 @@ import { colorElementRed, updateElementStyles } from "./utils.js";
 //   border: "2px solid green",
 // });
 
+/** ---------------------------------------------------------------------
+ *
+ * Make fale dark mode
+ *
+ */
+
+// const appContainer = document.getElementById("app");
+
+// const KEY_CODES = {
+//   D: 68,
+//   L: 76,
+// };
+
+// function keydownHandler(event) {
+//   // classList - https://developer.mozilla.org/en-US/docs/Web/API/Element/classList
+
+//   // console.log(event);
+
+//   if (event.keyCode === KEY_CODES.D) {
+//     // Make dark mode
+//     appContainer.classList.add('Fake-dark-mode')
+//     appContainer.classList.remove('Fake-light-mode')
+//   }
+
+//   if (event.keyCode === KEY_CODES.L) {
+//     // Remove dark mode
+//     appContainer.classList.add('Fake-light-mode')
+//     appContainer.classList.remove('Fake-dark-mode')
+//   }
+// }
+
+// document.addEventListener("keydown", keydownHandler);
